3461204647dfb71bd963e87bc085edc863ab4322
oshurek_m <thedarthset@gmail.com>
2022-10-18 15:46:32 +0300

    убрал асинхронность в def register_handler_client

--- handlers/client.py
+++ handlers/client.py
@@ -5,5 +5,5 @@ async def echo_send(message: types.Message):
     await message.reply(message.text)
 
 
-async def register_handler_client(dp_1: Dispatcher):
+def register_handler_client(dp_1: Dispatcher):
     dp_1.register_message_handler(echo_send)
