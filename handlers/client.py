36796f4151f29e05e7087538fe1de0563f2ae566
oshurek_m <thedarthset@gmail.com>
2022-10-25 17:46:43 +0300

    рабочий эхобот по поиску по итему. Выводит заказ, деталь, дату отгрузки, тп, примечания + количество. это все выделено жирным

--- handlers/client.py
+++ handlers/client.py
@@ -4,21 +4,20 @@ from create_bot import bot
 
 
 async def echo_send(message: types.Message):
-    await message.reply(message.text)
-    await print_result(message)
+    await bot.send_message(message.from_user.id, text='*5*', parse_mode='Markdown')
 
 
 async def item_filter(message: types.Message):
     item = int(message.text)
     exped = read_exped.exped
     result = read_exped.filter(df=exped, item=item)
-    text = f'Заказ: {result[0]}\n' \
-           f'Деталь: {result[1]}\n' \
-           f'Кол-во: {result[2]}\n' \
-           f'Дата отгрузки: {result[3]}\n' \
-           f'ТП: {result[5]}\n' \
-           f'Примечания: {result[4]}\n'
-    await bot.send_message(message.from_user.id, text)
+    text = f'Заказ: *{result[0]}*\n' \
+           f'Деталь: *{result[1]}*\n' \
+           f'Кол-во: *{result[2]}*\n' \
+           f'Дата отгрузки: *{result[3]}*\n' \
+           f'ТП: *{result[5]}*\n' \
+           f'Примечания: *{result[4]}*\n'
+    await bot.send_message(message.from_user.id, text=text, parse_mode='Markdown')
 
 
 async def print_result(message: types.Message):
@@ -27,4 +26,5 @@ async def print_result(message: types.Message):
 
 def register_handler_client(dp_1: Dispatcher):
     dp_1.register_message_handler(item_filter)
+    # dp_1.register_message_handler(echo_send)
 
