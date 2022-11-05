77e6b30dca9cb69ee359a29c1542b2bffc4bd1da
oshurek_m <thedarthset@gmail.com>
2022-11-05 11:50:32 +0300

    добавление фунции по фильтрации по номеру детали

--- database/read_exped.py
+++ database/read_exped.py
@@ -7,7 +7,7 @@ exped = pd.read_excel(
 )
 
 
-def filter(df, item: int):
+def filter_item(df, item: int):
     df_item = df[df['N° ITEM'] == item].reset_index(drop=True)
     df_item = df_item.loc[:, ['Индекс RTB',
                               'Наименование детали',
@@ -26,4 +26,28 @@ def filter(df, item: int):
                              'Примечания',
                              'Потенциальные проблемы'
                              ]].values
+    return inform
+
+
+def filter_detal(df, detal: str):
+    df_t = df['Наименование детали'].isin([detal])
+    df['search'] = df_t
+    df_detal = df[df['search'] == True]
+    df_detal = df_detal.loc[:, ['Индекс RTB',
+                                'Наименование детали',
+                                'Кол-во',
+                                'Неделя Отгрузки',
+                                'Примечания',
+                                'Отметка о выполнении',
+                                'Потенциальные проблемы',
+                                'N° ITEM',
+                                'Приоритет'
+                                ]].astype({'Индекс RTB': 'int64', 'Кол-во': 'int64', 'N° ITEM': 'int64'})
+    inform = df_detal.loc[:, ['Индекс RTB',
+                              'Наименование детали',
+                              'Кол-во',
+                              'Приоритет',
+                              'Примечания',
+                              'Потенциальные проблемы'
+                              ]].values
     return inform
